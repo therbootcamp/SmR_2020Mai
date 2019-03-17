13201b0c32a2c1cec33f74e9ae3d39cb500d866f
Dirk Wulff <dirk.wulff@gmail.com>
2019-03-17 14:50:59 +0100

    ewlkfj

---
+++ _sessions/RobustStats/code/avocado.R
@@ -0,0 +1,30 @@
+
+
+a = read_csv('~/Downloads/student-alcohol-consumption/student-mat.csv')
+b = read_csv('~/Downloads/student-alcohol-consumption/student-por.csv')
+
+dim(a)
+dim(b)
+a
+b
+
+
+with(a, plot(age, G1))
+
+
+a = read_csv('~/Downloads/avocado.csv')
+a = subset(a, year == 2017)
+
+r = lm(AveragePrice ~ Date,data=a)
+a$resid = r$residuals
+a$predi = predict(r)
+
+set.seed(27)
+ggplot(a[sample(1:nrow(a),200),], mapping = aes(Date, AveragePrice)) + geom_point() + theme_bw() +
+  labs(y = 'Average price') + 
+  geom_segment(aes(x = Date, xend=Date,y = AveragePrice, yend = predi),col='red')+
+  geom_smooth(method='lm', col = 'steelblue', lwd=2, se=FALSE)
+
+
+cor.test(formula = ChickWeight, data = ChickWeight)
+
