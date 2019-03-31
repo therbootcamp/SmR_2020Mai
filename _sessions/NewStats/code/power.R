606c9a6a8494d4940b6ef286da1607c4d36a0717
Dirk Wulff <dirk.wulff@gmail.com>
2019-03-31 16:06:35 +0200

    hm

--- _sessions/NewStats/code/power.R
+++ _sessions/NewStats/code/power.R
@@ -14,17 +14,17 @@ m2 = 120
 s1 = 25
 s2 = 25
 
-png('_sessions/NewStats/image/power.png',width=1200, height=800)
+png('_sessions/NewStats/image/power.png',width=1000, height=1000)
 
-par(mar=c(0,0,20,0))
+par(mar=c(0,0,30,0))
 
 plot.new();plot.window(xlim=c(0,200),ylim=c(0,.016))
 
-mtext('Small N', side=3, cex=6, col = Green, line=15, font=2)
+mtext('Small N', side=3, cex=6, col = Green, line=25, font=2)
 
 xs = seq(0,200,.01)
 
-lines(xs, dnorm(xs, m2, s2), lwd = 8, col = Pink)
+lines(xs, dnorm(xs, m2, s2), lwd = 12, col = Pink)
 
 xlim = qnorm(.95, m1, s1)
 xs = seq(0, xlim, .01)
@@ -32,7 +32,7 @@ ys = dnorm(xs, m2, s2)
 polygon(c(xs, rev(xs)),c(ys, rep(-.00015, length(ys))), border=NA, col=Pink)
 
 xs = seq(0,200,.01)
-lines(xs, dnorm(xs, m1, s1), lwd = 8, col = Gray)
+lines(xs, dnorm(xs, m1, s1), lwd = 12, col = Gray)
 
 
 xlim = qnorm(.95, m1, s1)
@@ -41,7 +41,7 @@ ys = dnorm(xs, m1, s1)
 polygon(c(xs, rev(xs)),c(ys, rep(-.00015, length(ys))), border=NA, col=Gray)
 
 text(100, .005, label = expression(beta), cex=8, col = 'white')
-text(129, .0009, label = expression(alpha), cex=8, col = 'white')
+text(129, .0008, label = expression(alpha), cex=8, col = 'white')
 text(61, .00523, label = expression(1-alpha), cex=4, col = Gray)
 text(139, .005, label = expression(1-beta), cex=4, col = Pink)
 
@@ -52,7 +52,7 @@ m2 = 120
 s1 = 12
 s2 = 12
 
-png('_sessions/NewStats/image/power_large.png',width=1200, height=800)
+png('_sessions/NewStats/image/power_large.png',width=1000, height=1000)
 
 par(mar=c(0,0,6,0))
 
@@ -62,7 +62,7 @@ mtext('Large N', side=3, cex=6, col = Green, line=1, font=2)
 
 xs = seq(0,200,.01)
 
-lines(xs, dnorm(xs, m2, s2), lwd = 8, col = Pink)
+lines(xs, dnorm(xs, m2, s2), lwd = 12, col = Pink)
 
 xlim = qnorm(.95, m1, s1)
 xs = seq(0, xlim, .01)
@@ -70,7 +70,7 @@ ys = dnorm(xs, m2, s2)
 polygon(c(xs, rev(xs)),c(ys, rep(-.0003, length(ys))), border=NA, col=Pink)
 
 xs = seq(0,200,.01)
-lines(xs, dnorm(xs, m1, s1), lwd = 8, col = Gray)
+lines(xs, dnorm(xs, m1, s1), lwd = 12, col = Gray)
 
 
 xlim = qnorm(.95, m1, s1)
@@ -78,12 +78,29 @@ xs = seq(xlim,200,.01)
 ys = dnorm(xs, m1, s1)
 polygon(c(xs, rev(xs)),c(ys, rep(-.0003, length(ys))), border=NA, col=Gray)
 
-text(95.8, .0013, label = expression(beta), cex=6, col = 'white')
-text(104.8, .0013, label = expression(alpha), cex=6, col = 'white')
+text(94.6, .0009, label = expression(beta), cex=6, col = 'white')
+text(105.6, .0009, label = expression(alpha), cex=6, col = 'white')
 text(75, .00523, label = expression(1-alpha), cex=4, col = Gray)
 text(125, .005, label = expression(1-beta), cex=4, col = Pink)
 
 dev.off()
+
+# memnet:::circle(.1,c(.5,.5))
+# 
+# memnet:::circle_raw(100,deg = 360, rad=.1, orig=c(.5, .5))
+# 
+# pwr::pwr.t.test(sig.level = .05, power = .95, d = 1)
+# 
+# png('_sessions/NewStats/image/blurr.png',width=1000,height=1200, bg='transparent')
+# plot.new();plot.window(c(0,1),c(0,1))
+# rs = seq(.2,2,.00015)
+# alphs = seq(.1,1,length.out = length(rs))
+# for(i in 1:length(rs)) {
+#   lines(memnet:::circle_raw(1000,deg = 360, rad=rs[i], orig=c(.5, .5)),col=rgb(0,0,0,alpha=alphs[i]))
+#   }
+# dev.off()
+
+
 
 
 
