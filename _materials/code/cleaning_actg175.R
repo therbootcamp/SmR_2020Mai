7db0e7e0eaf393c5b4d9da436fe965ce4e8b1bf0
Nathaniel Phillips <nathaniel.phillips@roche.com>
2019-04-05 22:17:16 +0200

    cleaned up ACTG175 data

--- _materials/code/cleaning_actg175.R
+++ _materials/code/cleaning_actg175.R
@@ -6,7 +6,7 @@ trial <- as_tibble(ACTG175)
 
 trial <- trial %>%
   mutate(hemo = factor(hemo, levels = c(0, 1), labels = c("No", "Yes")),
-         drugs = factor(drugs, levels = c(0, 1), labels = c("No", "Yes")),
+         drug_history = factor(drugs, levels = c(0, 1), labels = c("No", "Yes")),
          oprior = factor(oprior, levels = c(0, 1), labels = c("No", "Yes")),
          z30 = factor(z30, levels = c(0, 1), labels = c("No", "Yes")),
          homo = factor(homo, levels = c(0, 1), labels = c("No", "Yes")),
@@ -20,10 +20,24 @@ trial <- trial %>%
          treat = factor(treat, levels = c(0, 1), labels = c("Zidovudine only", "Other")),
          offtrt = factor(offtrt, levels = c(0, 1), labels = c("No", "Yes")),
          r = factor(r, levels = c(0, 1), labels = c("Missing", "Observed")),
-         arms = factor(arms, levels = c(0, 1, 2, 3), labels = c("zidovudine", "zidovudine and didanosine", 
-                                                                "zidovudine and zalcitabine", "didanosine"))) %>%
+         molecule = factor(arms, levels = c(0, 1, 2, 3), labels = c("Zidovudine", 
+                                                                          "zidovudine and Didanosine", 
+                                                                "Zidovudine and Zalcitabine", 
+                                                                "Didanosine"))) %>%
+  select(patient_id = pidnum, 
+         molecule, 
+         days, 
+         age, 
+         weight_kg = wtkg, 
+         drug_history, 
+         race, 
+         gender, 
+         symptomatic = symptom, 
+         cd4_baseline = cd40,
+         cd4_96_weeks = cd496,
+         cd8_baseline = cd80,
+         cd8_20_weeks = cd820,
+         ) %>%
   mutate_if(is.factor, paste)
-         
-         
 
 write_csv(trial, path = "1_Data/trial_ACTG175.csv")
